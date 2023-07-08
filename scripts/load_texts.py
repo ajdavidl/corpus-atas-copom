5185cf7f7012689e970cd519b7b32dc1e0f2dea2
Ajdavidl <78770941+ajdavidl@users.noreply.github.com>
2023-07-07 21:03:32 -0300

    It adds a python script with functions to load the texts.

---
+++ scripts/load_texts.py
@@ -0,0 +1,34 @@
+import os
+import re
+import pandas as pd
+import numpy as np
+
+
+def read_text_files():
+    listAtas = os.listdir("../atas")
+    corpus = []
+    meeting = []
+    for ata in listAtas:
+        meeting.append(int(re.search("[0-9]+", ata).group()))
+        with open("../atas/" + ata, 'rt', encoding='utf-8') as f:
+            lines = f.readlines()
+            if lines:
+                lines = ' '.join(lines)
+                corpus.append(lines)
+    return [corpus, meeting]
+
+
+def return_data_frame():
+    corpus, meeting = read_text_files()
+    dfCorpus = pd.DataFrame(corpus, index=meeting, columns=["text"])
+    del corpus, meeting
+    decisions = pd.read_csv("../decisions.csv")
+    decisions = decisions.dropna()
+    dfAux = decisions[decisions.meeting == 45]
+    decisions = decisions.drop(dfAux.index[1])
+    decisions.index = np.int64(decisions.meeting.values)
+    dfCorpus = dfCorpus.join(decisions)
+    dates = pd.read_csv("../copom_dates.csv")
+    dates.index = dates.minute
+    dfCorpus = dfCorpus.join(dates)
+    return dfCorpus
