d0588a5e21f2a2fbc4b86511c615171b83114e0d
Ajdavidl <78770941+ajdavidl@users.noreply.github.com>
2024-04-26 21:44:16 -0300

    Add new graphics to python parsing script.

--- scripts/parsing.py
+++ scripts/parsing.py
@@ -7,6 +7,112 @@ from collections import Counter
 
 from load_texts import *  # return_data_frame() and Mystopwords
 
+# Auxiliary functions
+def formality(spacyDoc):
+    ADJ = ADP = ADV = AUX = CONJ = CCONJ = DET = INTJ = NOUN = NUM = PART = 0
+    PRON = PROPN = PUNCT = SCONJ = SYM = VERB = X = SPACE = 0
+    for token in spacyDoc:
+        pos = token.pos_
+        if pos == 'NOUN':
+            NOUN = NOUN + 1
+        elif pos == 'ADP':
+            ADP = ADP + 1
+        elif pos == 'PUNCT':
+            PUNCT = PUNCT + 1
+        elif pos == 'DET':
+            DET = DET + 1
+        elif pos == 'VERB':
+            VERB = VERB + 1
+        elif pos == 'ADJ':
+            ADJ = ADJ +1
+        elif pos == 'PROPN':
+            PROPN = PROPN + 1
+        elif pos == 'NUM':
+            NUM = NUM + 1
+        elif pos == 'PRON':
+            PRON = PRON + 1
+        elif pos == 'ADV':
+            ADV = ADV +1
+        elif pos == 'AUX':
+            AUX = AUX + 1
+        elif pos == 'CONJ':
+            CONJ = CONJ + 1
+        elif pos == 'CCONJ':
+            CCONJ = CCONJ + 1
+        elif pos == 'INTJ':
+            INTJ = INTJ + 1
+        elif pos == 'PART':
+            PART = PART + 1
+        elif pos == 'SCONJ':
+            SCONJ = SCONJ + 1
+        elif pos == 'SYM':
+            SYM = SYM + 1
+        elif pos == 'X':
+            X = X + 1
+        elif pos == 'SPACE':
+            SPACE = SPACE + 1
+    F = NOUN + PROPN + ADJ + DET
+    C = PRON + ADV + VERB + AUX + INTJ
+    N = F + C + CONJ + CCONJ + SCONJ
+    if N == 0:
+        return (0)
+    else:
+        formality = 50 * ((F - C)/N + 1)
+        return(formality)
+
+
+def lexical_density(spacyDoc):
+    ADJ = ADP = ADV = AUX = CONJ = CCONJ = DET = INTJ = NOUN = NUM = PART = 0
+    PRON = PROPN = PUNCT = SCONJ = SYM = VERB = X = SPACE = 0
+    for token in spacyDoc:
+        pos = token.pos_
+        if pos == 'NOUN':
+            NOUN = NOUN + 1
+        elif pos == 'ADP':
+            ADP = ADP + 1
+        elif pos == 'PUNCT':
+            PUNCT = PUNCT + 1
+        elif pos == 'DET':
+            DET = DET + 1
+        elif pos == 'VERB':
+            VERB = VERB + 1
+        elif pos == 'ADJ':
+            ADJ = ADJ +1
+        elif pos == 'PROPN':
+            PROPN = PROPN + 1
+        elif pos == 'NUM':
+            NUM = NUM + 1
+        elif pos == 'PRON':
+            PRON = PRON + 1
+        elif pos == 'ADV':
+            ADV = ADV +1
+        elif pos == 'AUX':
+            AUX = AUX + 1
+        elif pos == 'CONJ':
+            CONJ = CONJ + 1
+        elif pos == 'CCONJ':
+            CCONJ = CCONJ + 1
+        elif pos == 'INTJ':
+            INTJ = INTJ + 1
+        elif pos == 'PART':
+            PART = PART + 1
+        elif pos == 'SCONJ':
+            SCONJ = SCONJ + 1
+        elif pos == 'SYM':
+            SYM = SYM + 1
+        elif pos == 'X':
+            X = X + 1
+        elif pos == 'SPACE':
+            SPACE = SPACE + 1
+    N = ADJ + ADP + ADV + AUX + CONJ + CCONJ + DET + INTJ + NOUN + NUM + PART + PRON + PROPN + SCONJ + VERB
+    if N == 0:
+        return (0)
+    else:
+        lex_den = (NOUN + PROPN + ADJ + ADV + VERB + AUX)/N
+        return(lex_den)    
+
+# Main code
+
 print("Loading data...")
 dfCorpus = return_data_frame()
 dfCorpus.text = dfCorpus.text.apply(lambda x : x.lower())
@@ -26,6 +132,8 @@ tag = []
 dep = []
 ent_text = []
 ent_label = []
+formality_ = []
+lexical_density_ = []
 for sentence in corpus:
     doc = nlp(sentence)
     for token in doc:
@@ -36,6 +144,8 @@ for sentence in corpus:
     for ent in doc.ents:
         ent_text.append(ent.text)
         ent_label.append(ent.label_)
+    formality_.append(formality(doc))
+    lexical_density_.append(lexical_density(doc))
 
 df = pd.DataFrame(list(zip(text, pos)),
                   columns=['word', 'pos'])
@@ -132,4 +242,26 @@ plt.bar(range(len(D)), list(D.values()),
         align='center')
 plt.xticks(range(len(D)), list(D.keys()), rotation=45)
 plt.title("Entities labels")
+plt.show()
+
+X = dfCorpus.meeting.to_list()
+
+plt.plot(X, formality_)
+plt.title("Formality")
+plt.xlabel("Meeting")
+plt.ylabel("Formality")
+plt.show()
+
+plt.boxplot(formality_)
+plt.title("Boxplot - Formality")
+plt.show()
+
+plt.plot(X, lexical_density_)
+plt.title("Lexical Density")
+plt.xlabel("Meeting")
+plt.ylabel("Lexical Density")
+plt.show()
+
+plt.boxplot(lexical_density_)
+plt.title("Boxplot - lexical_density")
 plt.show()
